22c47c86620af9340c9fc48ec355b88aed3961e4
Rafael Lima <rafaellimanf05@gmail.com>
2026-07-06 11:22:51 -0300

    configura auth-service com seguranca jwt e banco de dados

---
+++ auth-service/src/main/java/com/example/authservice/Models/User.java
@@ -0,0 +1,51 @@
+package com.example.authservice.Models;
+
+
+import jakarta.persistence.*;
+import org.springframework.stereotype.Controller;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String role;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
